4ffcb3158e82fdeb2d54aa28c0abe03564d4597d
Raghu Betina <raghu@firstdraft.com>
2019-12-01 18:07:04 +0000

    Whitelist columns in ResidencyProgram for ActiveAdmin

--- app/admin/residency_programs.rb
+++ app/admin/residency_programs.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register ResidencyProgram do
+
+ permit_params :state, :program_name
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
