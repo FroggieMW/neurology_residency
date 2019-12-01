fd1bf5a844c28d75d6fe25ce3aa1b43e789133e5
Raghu Betina <raghu@firstdraft.com>
2019-12-01 18:07:12 +0000

    Whitelist columns in InterviewInvite for ActiveAdmin

--- app/admin/interview_invites.rb
+++ app/admin/interview_invites.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register InterviewInvite do
+
+ permit_params :residency_program_id, :invite_received_date, :invitee_id
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
